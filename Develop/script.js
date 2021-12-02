9e4728f9936da365452db8476782de67157c0b97
Jacob Liberty <jacob.tobin.liberty@gmail.com>
2021-12-02 15:36:26 -0500

    added char choices, and password generator

--- Develop/script.js
+++ Develop/script.js
@@ -12,33 +12,52 @@ special = [
   "!", "#", "$", "%", "&", "'", '"', "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"
 ]
 
-console.log(lower)
-console.log(upper)
-console.log(number)
-console.log(special)
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   //Ask for length of password
-  console.log("pressed")
-  var length = ""
-  while (length < 8) {
+  var length = 0;
+  while (length < 8 || length > 128) {
     length = parseInt(prompt("How long should the password be?"));
-    if (length < 8) {
-      window.alert("password must be 8 charachters or longer")
+    if (length < 8 || length > 128) {
+      window.alert("password must be atleast 8 characters long but less than 128");
     }
   }
+  // Ask what type of characters are desired and create a list of all characters
+  var available = []
+  var L = confirm("Would you like to include Lowercase characters");
+  var U = confirm("Would you like to include Uppercase characters");
+  var N = confirm("Would you like to include Numbers");
+  var S = confirm("Would you like to include Special characters");
   
+  if (L === true) {
+    available = available.concat(lower);
+  }
+  if (U === true) {
+    available = available.concat(upper);
+  }
+  if (N === true) {
+    available = available.concat(number);
+  }
+  if (S === true) {
+    available = available.concat(special);
+  }
 
+  if (L === false && U === false && N === false && S === false) {
+    window.alert("You must choose atleast one type of characters");
+    writePassword();
+  }
+
+  // Ramdom password generator 
+  var password = "";
+  var i = 0;
+  while (i < length) {
+    password = password + available[Math.floor(Math.random()*available.length)];
+    i++;
+  }
+
+  //Change Placeholder text to new password
+  document.getElementById("password").innerHTML = password;
 }
-
-function test() {
-  console.log("button pressed")
-}
-
-// Add event listener to generate button
-
